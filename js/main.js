8b02b5df2b72fc6750191ae449d9a490dd1eff0e
Valerio <canivalerio93@gmail.com>
2022-07-11 14:05:23 +0200

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 //Consegna:
 //Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
+const ArrayLista = ['latte','caffe','farina','passata di pomodoro','frutta','sale','fagioli','fazzoletti'];
+let lista = document.getElementById('lista');
+let i=0;
+
+while (i<ArrayLista.length){
+    lista.innerHTML += `<li>${ArrayLista[i]}</li>`;
+    i++;
+};
